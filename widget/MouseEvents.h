01e7360d6ceffe5b1043f39e44f6ecc3d775b99b
playwright-devops <devops@playwright.dev>
2025-11-04 15:24:39 -0800

    3-way conflicts

--- widget/MouseEvents.h
+++ widget/MouseEvents.h
@@ -386,6 +386,9 @@ class WidgetMouseEvent : public WidgetMouseEventBase,
   // Otherwise, this must be 0.
   uint32_t mClickCount = 0;
 
+  // Unique event ID
+  uint32_t mJugglerEventId = 0;
+
   // Whether the event should ignore scroll frame bounds during dispatch.
   bool mIgnoreRootScrollFrame = false;
 
@@ -425,6 +428,7 @@ class WidgetMouseEvent : public WidgetMouseEventBase,
     mContextMenuTrigger = aEvent.mContextMenuTrigger;
     mExitFrom = aEvent.mExitFrom;
     mClickCount = aEvent.mClickCount;
+    mJugglerEventId = aEvent.mJugglerEventId;
     mIgnoreRootScrollFrame = aEvent.mIgnoreRootScrollFrame;
     mIgnoreCapturingContent = aEvent.mIgnoreCapturingContent;
     mClickEventPrevented = aEvent.mClickEventPrevented;
--- widget/nsGUIEventIPC.h
+++ widget/nsGUIEventIPC.h
@@ -252,6 +252,7 @@ struct ParamTraits<mozilla::WidgetMouseEvent> {
     }
     WriteParam(aWriter, aParam.mClickCount);
     WriteParam(aWriter, aParam.mCallbackId);
+    WriteParam(aWriter, aParam.mJugglerEventId);
 
     // Mark the event as stopped to notify callback.
     const_cast<mozilla::WidgetMouseEvent&>(aParam).mCallbackId.reset();
@@ -280,6 +281,7 @@ struct ParamTraits<mozilla::WidgetMouseEvent> {
     }
     rv = rv && ReadParam(aReader, &aResult->mClickCount) &&
          ReadParam(aReader, &aResult->mCallbackId);
+    rv = rv && ReadParam(aReader, &aResult->mJugglerEventId);
     return rv;
   }
 };
